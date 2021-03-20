5c2e7ef0a585797ae5f23ac923bd02a9e4c42c76
Luke <constableldp@gmail.com>
2021-03-20 12:42:55 +0000

    added gym class and tests checked and name fixed

---
+++ models/gym_class.py
@@ -0,0 +1,9 @@
+class GymClass:
+
+    def __init__(self, name, description, min_time, max_time, min_capacity, max_capacity):
+        self.name = name
+        self.description = description
+        self.min_time = min_time
+        self.max_time = max_time
+        self.min_capacity = min_capacity
+        self.max_capacity = max_capacity
--- run_test.py
+++ run_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tests.member_test import TestMember
+from tests.gym_class_test import TestGymClass
 
 if __name__ == '__main__':
     unittest.main()
