407f9140e3af2969b1d42d770dd684d3763b197f
Luke <constableldp@gmail.com>
2021-03-20 14:46:38 +0000

    added room class and ran tests

---
+++ models/room.py
@@ -0,0 +1,6 @@
+class Room:
+
+    def __init__(self, name, capacity, descripton = None):
+        self.name = name
+        self.capacity = capacity
+        self.description = descripton
--- run_test.py
+++ run_test.py
@@ -3,6 +3,7 @@ import unittest
 from tests.member_test import TestMember
 from tests.gym_class_test import TestGymClass
 from tests.instructor_test import TestInstructorDetails, TestInstructorSchedule
+from tests.room_test import TestRoom
 
 if __name__ == '__main__':
     unittest.main()
