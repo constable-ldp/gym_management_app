920eedbb63aa5977eb4d72a54de738caeaae98af
Luke <constableldp@gmail.com>
2021-03-21 09:17:50 +0000

    added index to class controller

---
+++ controllers/class_controller.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+from models.gym_class import GymClass
+import repositories.gym_class_repository as class_repository
+
+classes_blueprint = Blueprint('classes', __name__)
+
+@classes_blueprint.route('/classes')
+def classes():
+    classes = class_repository.select_all90
+    return render_template('classes/index.html', classes=classes)
