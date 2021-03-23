8223d5867226f749f83ed799c941e6309dd915d4
Luke <constableldp@gmail.com>
2021-03-23 08:50:42 +0000

    created members new page

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 from models.member import Member
 import repositories.member_repository as member_repository
+import datetime
 
 members_blueprint = Blueprint('members', __name__)
 
@@ -38,3 +39,36 @@ def edit_member(id):
                     premium, member_since, member_until, id)
     member_repository.update(member)
     return redirect('/members')
+
+@members_blueprint.route('/members/new')
+def new_member():
+    return render_template('members/new.html')
+
+@members_blueprint.route('/members/new', methods=['POST'])
+def add_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    date_of_birth = request.form['date_of_birth']
+    email = request.form['email']
+    phone = request.form['phone']
+    if request.form.get('membership'):
+        membership = True
+        member_since = request.form['member_since']
+        member_until = request.form['member_until']
+        if request.form.get('premium'):
+            premium = True
+        else:
+            premium = False
+    else:
+        membership = False
+        premium = False
+        member_since = None
+        member_until = None
+    member = Member(first_name, last_name, email, phone, date_of_birth, membership,
+                    premium, member_since, member_until, id)
+    member_repository.save(member)
+    return redirect('/members')
+
+@members_blueprint.route('/members/<id>/delete', methods=['POST'])
+def delete_member(id):
+    
