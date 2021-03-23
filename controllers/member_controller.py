cec65bb40c2d99950c019102f1a1c4594e911415
Luke <constableldp@gmail.com>
2021-03-23 09:01:00 +0000

    added delete functionality

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -71,4 +71,5 @@ def add_member():
 
 @members_blueprint.route('/members/<id>/delete', methods=['POST'])
 def delete_member(id):
-    
+    member_repository.delete(id)
+    return redirect('/members')
